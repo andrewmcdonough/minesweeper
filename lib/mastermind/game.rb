5d740745805b909cc811bf57c7f11aa10a5c6d59
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2010-01-10 23:28:07 +0000

    added files

---
+++ lib/mastermind/game.rb
@@ -0,0 +1,25 @@
+module Mastermind
+  class Game
+    def initialize(messenger)
+      @messenger = messenger
+    end
+
+     def start(code)
+       @code = code
+       @messenger.puts "Welcome to Mastermind!"
+       @messenger.puts "Enter guess:"
+     end
+
+     def guess(guess)
+      result = []
+      guess.each_with_index do |peg,index|
+        if @code[index] == peg
+          result << "b"
+        elsif @code.include?(peg)
+          result << "w"     
+        end
+      end
+      @messenger.puts result.sort.join
+    end
+  end
+end
